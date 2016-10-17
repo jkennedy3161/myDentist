880f5ed075341cffcaa86d353a33060585c1a4c7
jkennedy3161 <kennedyjt88@gmail.com>
2016-10-17 13:51:38 -0700

    'Able to serve up a static index.html from the public folder with express.'

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+// tell express to treat public folder as a static folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Listen for requests
+app.listen(8080);
+
+console.log('Connected to your server');
 
 app.get('/', function(req, res) {
 	res.write('server is connected!');
 	res.end();
 });
-
-app.listen(8080);
-
-console.log('Connected to your server');
