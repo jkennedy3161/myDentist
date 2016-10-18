0838420dc7336c5ba33bc972cb10e7942333c633
jkennedy3161 <kennedyjt88@gmail.com>
2016-10-17 17:25:51 -0700

    'Built out more of a proper skeleton for my app. Server still runs.'

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express(); 
+
+var port = process.env.PORT || 8080;        // set our port
+
+// configure our server with all the middleware and routing
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+// Listen for requests
+app.listen(port);
+
+console.log('Connected to your server');
+
